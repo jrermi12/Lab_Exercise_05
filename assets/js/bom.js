dcdc543b2a820cc64b631a586d90b37c2b514708
jrermi <it.ermias.asmare@gmail.com>
2021-02-01 01:07:57 -0500

    created a bom.html and bom.js and finished the task

---
+++ assets/js/bom.js
@@ -0,0 +1,7 @@
+const app = document.querySelectorAll('span .badge')
+
+app[0].innerHTML= window.location.href
+app[1].innerHTML= window.location.protocol
+app[2].innerHTML= window.location.host
+app[3].innerHTML= window.location.port
+app[4].innerHTML= window.location.hostname
